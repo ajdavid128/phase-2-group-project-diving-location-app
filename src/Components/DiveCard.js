5ff154eae7bc76e595f23edced83e014e89a90f8
julia <juliajaj3@gmail.com>
2022-11-15 17:32:26 -0500

    ternanry

--- src/Components/DiveCard.js
+++ src/Components/DiveCard.js
@@ -2,10 +2,13 @@ import CardDetails from "./CardDetails"
 
 
 function DiveCard({dive}) {
+function handleMouseOver = () => {
+  console.log("ternary")
+}
 console.log (dive)
     return (
       <div className="card">
-        <img className="card-image" src={dive.image} alt={dive.reef}/>
+        <img onmouseover= {handleMouseOver} className="card-image" src={dive.image} alt={dive.reef}/>
         <div className="container">
           <h4>{dive.reef}</h4>
           <h6>{dive.country}</h6>

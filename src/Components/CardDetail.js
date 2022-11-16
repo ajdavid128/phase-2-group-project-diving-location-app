66820853c121a3418763e65e1d8e61724f0e2ec2
Aaron David <ajdavid128@gmail.com>
2022-11-15 22:25:48 -0600

    filled out useState, useEffect, fetch request, deconstruction of diveSite -> all in CardDetails

--- src/Components/CardDetail.js
+++ src/Components/CardDetail.js
@@ -4,8 +4,23 @@ import {useParams} from "react-router-dom";
 
 function CardDetails() {
     
-    const [] = useState()
-    const [] = useState()
+    const [diveSite, setDiveSite] = useState(null)
+    const [isLoaded, setIsLoaded] = useState(false)
+
+    const {id} = useParams();
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/suggestions/${id}`)
+        .then(r => r.json())
+        .then(location => {
+            setDiveSite(location)
+            setIsLoaded(true)
+        });
+    },[id])
+
+    if (!isLoaded) return <h2>Loading...</h2>;
+
+    const {reef, country, location_description, dive_summary, depth, visibility, currents, surface_conditions, water_temp, experience_level, number_of_dive_sites, diving_season, image, url} = diveSite;
 
     return (
         <div>

a3ff3c28abbc571ca1f77cd9b45ac06199add024
Aaron David <ajdavid128@gmail.com>
2022-11-17 15:07:34 -0600

    minor changes

--- src/Components/SuggestionCard.js
+++ src/Components/SuggestionCard.js
@@ -4,8 +4,8 @@ function SuggestionCard({id, reef, country, image, url}) {
 
 
     return (
-        <div className="suggest-card">
-            <img className="card-img" src={image} alt={reef}/>
+        <div className="card">
+            <img className="card-image" src={image} alt={reef}/>
             <h3>{reef}</h3>
             <h4>{country}</h4>
         </div>

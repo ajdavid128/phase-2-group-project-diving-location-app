b6c035a4248e5d3a0c473eccf6ae3b8049a4e06b
Aaron David <ajdavid128@gmail.com>
2022-11-18 08:51:41 -0600

    CSS change

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -4,7 +4,6 @@ function Search({search, setSearch}) {
 
     return (
       <div id="search-container">
-        {/* <label>Search</label>  */}
         <input className="searchbar" 
         type="text" 
         placeholder="Look Up Country or Reef" 
